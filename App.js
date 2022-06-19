97712d32fc5970faa32a56bad634a75f6e4a8712
Faizan59khan <khan_faizan59@yahoo.com>
2022-06-19 15:20:38 +0500

    name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Faizan</Text>
+      <Text>Hello Faizan Khan</Text>
       <StatusBar style="auto" />
     </View>
   );
