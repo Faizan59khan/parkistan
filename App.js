ec1f2e8c07745d27d125d7617b8b1d7bca9c0dde
Maham-Anwar <mahamanwar17@gmail.com>
2022-06-20 13:09:01 +0500

    App.js

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Faizan</Text>
+      <Text>Hello Maham Anwar</Text>
+
       <StatusBar style="auto" />
     </View>
   );
