c85878bf02f86b71ec17a6fed20837946dd13724
Maham-Anwar <mahamanwar17@gmail.com>
2022-07-29 20:19:23 +0500

    Home page & side drawer completed

--- screens/RootStackScreen.js
+++ screens/RootStackScreen.js
@@ -4,9 +4,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from 'react-native-splash-screen';
 import LoginScreen from './.././pages/login/index';
 import UserSignUpScreen from './../pages/signup/UserForm';
-import OwnerSignUpScreen from './../pages/signup/OwnerForm'
-import Map from '../Features/QuickPark'
+import OwnerSignUpScreen from './../pages/signup/OwnerForm';
+import HomePage from '../pages/HomePage';
+import Map from '../Features/QuickPark';
 import Locations from '../pages/Location';
+import DrawerScreen from './DrawerScreen';
 const RootStack = createNativeStackNavigator();
 
 const RootStackScreen = () => {
@@ -15,10 +17,11 @@ const RootStackScreen = () => {
     screenOptions={{
       headerShown: false
     }}>
-      {/* <RootStack.Screen name='SplashScreen' component={SplashScreen}/> */}
+
       <RootStack.Screen  name='LoginScreen' component={LoginScreen}/>
       <RootStack.Screen name='UserSignUpScreen' component={UserSignUpScreen}/>
       <RootStack.Screen name='OwnerSignUpScreen' component={OwnerSignUpScreen}/>
+      <RootStack.Screen name='HomePage' component={DrawerScreen}/>
        <RootStack.Screen name='Locations' component={Locations}/>
        <RootStack.Screen name='Map' component={Map}/>
     </RootStack.Navigator>
